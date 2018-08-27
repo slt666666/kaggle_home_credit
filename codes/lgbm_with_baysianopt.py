566669ecec89bd8ad5637212247b27e1cd7479db
堺俊之 <toshi6661024@gmail.com>
2018-08-27 16:34:45 +0900

    fix bug

--- codes/lgbm_with_baysianopt.py
+++ codes/lgbm_with_baysianopt.py
@@ -116,7 +116,7 @@ def application_train_test(file_path = file_path, nan_as_category = True):
     for col in ['FONDKAPREMONT_MODE', 'HOUSETYPE_MODE', 'WALLSMATERIAL_MODE', 'EMERGENCYSTATE_MODE', 'OBS_30_CNT_SOCIAL_CIRCLE', 'DEF_30_CNT_SOCIAL_CIRCLE', 'OBS_60_CNT_SOCIAL_CIRCLE', 'DEF_60_CNT_SOCIAL_CIRCLE',
            'NAME_CONTRACT_TYPE', 'CODE_GENDER', 'FLAG_OWN_CAR', 'FLAG_OWN_REALTY', 'NAME_TYPE_SUITE', 'NAME_INCOME_TYPE', 'NAME_EDUCATION_TYPE', 'NAME_FAMILY_STATUS', 'NAME_HOUSING_TYPE', 'OCCUPATION_TYPE',
             'WEEKDAY_APPR_PROCESS_START', 'ORGANIZATION_TYPE', 'WEEKDAY_APPR_PROCESS_START']:
-        unique_values = list(set(list(application_train[col].astype(str).unique()) + list(application_test[col].astype(str).unique())))
+        unique_values = list(set(df[col].astype(str).unique()))
         le.fit(unique_values)
         df[col] = le.transform(df[col].astype(str))
 
